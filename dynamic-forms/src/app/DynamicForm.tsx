7b8934897bd9fbc3cfc04dfa88205b3ae90cd4ca
Yazan Amer <yazan.amer@protonmail.com>
2025-02-01 18:54:49 +0300

    Use react hook forms

--- dynamic-forms/src/app/DynamicForm.tsx
+++ dynamic-forms/src/app/DynamicForm.tsx
@@ -1,8 +1,9 @@
 "use client";
+import { register } from "module";
 import { useState } from "react";
-import { ASSIGNMENT_SAMPLE_DATA } from "./sample_data";
+import { useForm, UseFormRegister } from "react-hook-form";
 
-function parseFormElement(elementData: {
+type ElementData = {
   id: number;
   name: string;
   fieldType: string;
@@ -11,102 +12,123 @@ function parseFormElement(elementData: {
   listOfValues1?: string[];
   minLength?: number;
   maxLength?: number;
-}) {
+};
+
+function parseFormElement(
+  elementData: ElementData,
+  register: UseFormRegister<any>
+) {
   switch (elementData.fieldType) {
     case "TEXT":
-      return TextFormElement(elementData);
+      return <TextFormElement register={register} elementData={elementData} />;
     case "LIST":
-      const options = elementData.listOfValues1 || [];
-      const dataListName = elementData.name;
-      return (
-        <>
-          <input
-            className="form-control"
-            required={elementData.required}
-            list={dataListName}
-            name={dataListName}
-          ></input>
-          <label htmlFor={dataListName}>{dataListName}</label>
-          <datalist id={dataListName}>
-            {options.map((option) => (
-              <option key={option} value={option} />
-            ))}
-          </datalist>
-        </>
-      );
+      return <ListFormElement register={register} elementData={elementData} />;
     case "RADIO":
-      return RadioFormElement();
+      return <RadioFormElement elementData={elementData} register={register} />;
     default:
       return <>Unknown form element</>;
   }
-
-  function RadioFormElement() {
-    const [selected, setSelected] = useState(elementData.defaultValue);
-    const radioOptions = elementData.listOfValues1 ?? [];
-    return (
-      <>
-        <label className="form-label" htmlFor={elementData.name}>
-          {elementData.name}
-        </label>
-        {radioOptions.map((option, index) => {
-          return (
-            <div key={option} className="form-check">
-              <input
-                onChange={() => {
-                  const selected = index.toString();
-                  setSelected(selected);
-                }}
-                className="form-check-input"
-                type="radio"
-                name={option}
-                id={option}
-                value={index}
-                checked={index.toString() === selected}
-              />
-              <label className="form-check-label" htmlFor="customRadio1">
-                {option}
-              </label>
-            </div>
-          );
-        })}
-      </>
-    );
-  }
 }
 
-export const DynamicForm: React.FC = () => {
-  return (
-    <div className="container d-flex justify-content-center m-5">
-      <form className="w-50">
-        {ASSIGNMENT_SAMPLE_DATA.data.map((elementData) => (
-          <div className="row mb-3" key={elementData.id}>
-            {parseFormElement(elementData)}
-          </div>
-        ))}
-      </form>
-    </div>
-  );
-};
-function TextFormElement(elementData: {
-  id: number;
-  name: string;
-  fieldType: string;
-  defaultValue: string;
-  required: boolean;
-  listOfValues1?: string[];
-  minLength?: number;
-  maxLength?: number;
-}) {
+const ListFormElement: React.FC<{
+  elementData: ElementData;
+  register: UseFormRegister<any>;
+}> = (props) => {
+  const options = props.elementData.listOfValues1 || [];
+  const dataListName = props.elementData.name;
   return (
     <>
       <input
         className="form-control"
-        required={elementData.required}
-        name={elementData.name}
-        defaultValue={elementData.defaultValue}
-        type="text"
+        required={props.elementData.required}
+        list={dataListName}
+        name={dataListName}
       ></input>
-      <label htmlFor={elementData.name}> {elementData.name} </label>
+      <label htmlFor={dataListName}>{dataListName}</label>
+      <datalist id={dataListName}>
+        {options.map((option) => (
+          <option key={option} value={option} />
+        ))}
+      </datalist>
     </>
   );
-}
+};
+
+const RadioFormElement: React.FC<{
+  elementData: ElementData;
+  register: UseFormRegister<any>;
+}> = (props) => {
+  const [selected, setSelected] = useState(props.elementData.defaultValue);
+  const radioOptions = props.elementData.listOfValues1 ?? [];
+  return (
+    <>
+      <label className="form-label" htmlFor={props.elementData.name}>
+        {props.elementData.name}
+      </label>
+      {radioOptions.map((option, index) => {
+        return (
+          <div key={option} className="form-check">
+            <input
+              {...props.register(props.elementData.name)}
+              onChange={() => {
+                const selected = index.toString();
+                setSelected(selected);
+              }}
+              className="form-check-input"
+              type="radio"
+              name={option}
+              id={option}
+              value={index}
+              checked={index.toString() === selected}
+            />
+            <label className="form-check-label" htmlFor="customRadio1">
+              {option}
+            </label>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+const TextFormElement: React.FC<{
+  elementData: ElementData;
+  register: UseFormRegister<any>;
+}> = (props) => {
+  return (
+    <>
+      <input
+        {...props.register(props.elementData.name)}
+        className="form-control"
+        required={props.elementData.required}
+        name={props.elementData.name}
+        defaultValue={props.elementData.defaultValue}
+        type="text"
+      ></input>
+      <label htmlFor={props.elementData.name}> {props.elementData.name} </label>
+    </>
+  );
+};
+
+export const DynamicForm: React.FC<{ formDesc: string }> = (props) => {
+  const { register, handleSubmit } = useForm();
+  const parsed: { data: ElementData[] } = JSON.parse(props.formDesc);
+
+  return (
+    <div className="container d-flex justify-content-center m-5">
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+        className="w-50"
+      >
+        {parsed.data.map((elementData) => (
+          <div className="row mb-3" key={elementData.id}>
+            {parseFormElement(elementData, register)}
+          </div>
+        ))}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
--- dynamic-forms/src/app/page.tsx
+++ dynamic-forms/src/app/page.tsx
@@ -1,11 +1,42 @@
+"use client";
+import { useState } from "react";
 import { DynamicForm } from "./DynamicForm";
 import styles from "./page.module.css";
+import { ASSIGNMENT_SAMPLE_DATA } from "./sample_data";
+
+const INITIAL_INPUT = JSON.stringify(ASSIGNMENT_SAMPLE_DATA);
+
+const FormManipulator: React.FC<{
+  setFormDesc: (formDesc: string) => void;
+}> = (props) => {
+  const [value, setValue] = useState(INITIAL_INPUT);
+
+  return (
+    <>
+      <input
+        style={{
+          width: "50vw",
+          height: "25vh",
+        }}
+        type="text"
+        value={value}
+        onChange={(event) => {
+          event.stopPropagation();
+          console.log("Changed the value!");
+          setValue(event.currentTarget.value);
+          props.setFormDesc(event.currentTarget.value);
+        }}
+      ></input>
+    </>
+  );
+};
 
 export default function Home() {
-  
+  const [formDesc, setFormDesc] = useState(INITIAL_INPUT);
   return (
     <div className={styles.page}>
-      <DynamicForm />
+      <DynamicForm formDesc={formDesc} />
+      <FormManipulator setFormDesc={setFormDesc} />
     </div>
   );
 }
